cc83929c5ac8a17732b946dbeef1c2a18314be14
Avrach <39876767+Avrach@users.noreply.github.com>
2025-11-04 13:15:28 +0000

    Add files via upload

---
+++ practical_3.R
@@ -0,0 +1,15 @@
+# Github Link: https://github.com/Avrach/Practical_3
+
+#Name:Yuyang Zhang  ID:S2808557
+#Name:Qihong Xie  ID:S2792875
+#Name:Yujie Sun  ID:S2805445
+
+#Contribution
+#Yuyang Zhang completed % of the work.
+#Qihong Xie completed % of the work.
+#Yujie Sun completed % of the work.
+
+#
+#
+#
+
